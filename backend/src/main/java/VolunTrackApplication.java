77debc79da2574dca764d6978b8ca88c583b47a7
Rudra Agrawal <rudraagrawal191@gmail.com>
2026-05-05 15:45:58 +0530

    java folder

---
+++ backend/src/main/java/VolunTrackApplication.java
@@ -0,0 +1,11 @@
+package com.voluntrack.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VolunTrackApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(VolunTrackApplication.class, args);
+  }
+}
